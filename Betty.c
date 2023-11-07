ec423680df2d1e42b69fef872e54d3d1d5c90492
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-11-06 21:09:11 -0800

    Betty.c

---
+++ Betty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of the program
+ *
+ *Description: this is a simple_shell program that cling
+ *to the betty coding style
+ *
+ *Return: 0 (success)
+ */
+int main (void)
+{
+	printf("Hello, Betty!\n",);
+	return (0);
+}
