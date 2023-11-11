74a9a8b079054beaa3cde2e6d0d963cbf96c0494
Mmathapelo Sebela <mathepelofentse@icloud.com>
2023-11-11 01:43:35 -0800

    excbid.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <unistd.h>
 
 /**
  * main - the main function
--- shell.h
+++ shell.h
@@ -6,7 +6,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+
+
 
 void rncmd(char **arv, char **envp);
-
+int excute_command(int status, char *args[]);
 #endif
