34f2a4e1b00c65e1d8b013d450cbeb7b400a0851
Heshbon <cityalight.hesbon@gmail.com>
2023-11-15 16:48:41 -0800

    shell.h

--- shell.h
+++ shell.h
@@ -10,27 +10,19 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-extern char **environ;
-
 #define MAX_COMMAND_LENGTH 256
 #define MAX_ARG_COUNT 64
+
 /**
  *main - main function
  *dsp_promt - function that display promts
  *excute_command - excute promts
  *Return: void
  */
+
 int main(void);
 void display_prompt(void);
 int excute_command(char *comand);
 void resolve_command_line(char *command_line, char **args);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-int _strcmp(char *str1, char *str2);
-char *_strdup(char *s);
-int _strlen(char *c);
-char *_getenv(char *var);
-int print_env(void);
-int _str_cmp(char *st1, char *st2, int n);
 
 #endif
